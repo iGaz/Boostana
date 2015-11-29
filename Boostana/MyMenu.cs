ddc19455d3ece302519b4cb93ef350b5244826f1
iRaxe <djvinx@live.com>
2015-11-29 12:34:57 +0100

    Update MyMenu.cs

--- Boostana/MyMenu.cs
+++ Boostana/MyMenu.cs
@@ -134,6 +134,18 @@ namespace Boostana
             MyActivator.Add("youmus.Enemies",
                 new Slider("Use Youmus Ghostblade when there are {0} enemies in range", 3, 1, 5));
             MyActivator.AddSeparator();
+            MySpells.AddGroupLabel("Potion Settings");
+            MySpells.Add("spells.Corrupt.Check", new CheckBox("Corrupt Potion"));
+            MySpells.Add("spells.Corrupt.HP", new Slider("Use Corrupt Potion when HP is lower than {0}(%)", 60, 1, 100));
+            MySpells.Add("spells.Corrupt.Mana", new Slider("Use Corrupt Potion when Mana is lower than {0}(%)", 60, 1, 100));
+            MySpells.AddSeparator();
+            MySpells.Add("spells.Hunter.Check", new CheckBox("Hunter's Potion"));
+            MySpells.Add("spells.Hunter.HP", new Slider("Use Hunter's Potion when HP is lower than {0}(%)", 60, 1, 100));
+            MySpells.Add("spells.Hunter.Mana", new Slider("Use Hunter's Potion when Mana is lower than {0}(%)", 60, 1, 100));
+            MySpells.AddSeparator();
+            MySpells.Add("spells.HealPot.Check", new CheckBox("Health Potion"));
+            MySpells.Add("spells.HealPot.HP", new Slider("Use Heal Potion when Hp is lower than {0}(%)", 60, 1, 100));
+            MySpells.AddSeparator();
             MySpells = MyMenu.AddSubMenu("Spells Settings");
             MySpells.AddGroupLabel("Spells settings:");
             MySpells.AddGroupLabel("Heal settings:");
@@ -337,7 +349,38 @@ namespace Boostana
         {
             return MyActivator["items.Youmuss.HP"].Cast<Slider>().CurrentValue;
         }
-
+        public static bool SpellsCorruptCheck()
+        {
+            return MySpells["spells.Corrupt.Check"].Cast<CheckBox>().CurrentValue;
+        }
+        public static float SpellsCorruptHP()
+        {
+            return MySpells["spells.Corrupt.HP"].Cast<Slider>().CurrentValue;
+        }
+        public static float SpellsCorruptMana()
+        {
+            return MySpells["spells.Corrupt.Mana"].Cast<Slider>().CurrentValue;
+        }
+        public static bool SpellsHunterCheck()
+        {
+            return MySpells["spells.Hunter.Check"].Cast<CheckBox>().CurrentValue;
+        }
+        public static float SpellsHunterMana()
+        {
+            return MySpells["spells.Hunter.Mana"].Cast<Slider>().CurrentValue;
+        }
+        public static float SpellsHunterHP()
+        {
+            return MySpells["spells.Hunter.HP"].Cast<Slider>().CurrentValue;
+        }
+        public static bool SpellsHealPotCheck()
+        {
+            return MySpells["spells.HealPot.Check"].Cast<CheckBox>().CurrentValue;
+        }
+        public static float SpellsHealPotHP()
+        {
+            return MySpells["spells.HealPot.HP"].Cast<Slider>().CurrentValue;
+        }
         public static float SpellsHealHp()
         {
             return MySpells["spells.Heal.HP"].Cast<Slider>().CurrentValue;
