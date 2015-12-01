2a1bf1ce691f6768c7bd5cdee2577d7bb1dfe815
iRaxe <djvinx@live.com>
2015-12-01 18:08:55 +0100

    Update MyActivator.cs

--- Boostana/MyActivator.cs
+++ Boostana/MyActivator.cs
@@ -6,15 +6,18 @@ namespace Boostana
     internal static class MyActivator
     {
         public static Spell.Targeted Ignite;
-        public static Item Youmus, Botrk, Bilgewater, CorruptPot, HuntersPot, RefillPotion;
-        public static Spell.Active Heal;
+        public static Item Youmus, Botrk, Bilgewater, CorruptPot, HuntersPot, RefillPot, Biscuit, HPPot;
+        public static Spell.Active Heal, Barrier;
         public static void LoadSpells()
         {
             if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner1).Name.Contains("dot"))
                 Ignite = new Spell.Targeted(SpellSlot.Summoner1, 580);
             else if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner2).Name.Contains("dot"))
                 Ignite = new Spell.Targeted(SpellSlot.Summoner2, 580);
-
+            if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner1).Name.Contains("barrier"))
+                Barrier = new Spell.Active(SpellSlot.Summoner1);
+            else if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner2).Name.Contains("barrier"))
+                Barrier = new Spell.Active(SpellSlot.Summoner2);
             if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner1).Name.Contains("heal"))
                 Heal = new Spell.Active(SpellSlot.Summoner1);
             else if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner2).Name.Contains("heal"))
@@ -22,9 +25,11 @@ namespace Boostana
             Youmus = new Item((int)ItemId.Youmuus_Ghostblade);
             Botrk = new Item((int)ItemId.Blade_of_the_Ruined_King);
             Bilgewater = new Item((int)ItemId.Bilgewater_Cutlass);
+            HPPot = new Item(2003);
+            Biscuit = new Item(2010);
             CorruptPot = new Item(2033);
             HuntersPot = new Item(2032);
-            RefillPotion = new Item(2031);
+            RefillPot = new Item(2031);
         }
     }
 }
