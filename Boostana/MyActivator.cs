3c84d26095b48f592a96838559dbb8e5df52838e
iRaxe <djvinx@live.com>
2015-12-08 02:39:25 +0100

    Update MyActivator.cs

--- Boostana/MyActivator.cs
+++ Boostana/MyActivator.cs
@@ -6,8 +6,24 @@ namespace Boostana
     internal static class MyActivator
     {
         public static Spell.Targeted Ignite;
-        public static Item Youmus, Botrk, Bilgewater, CorruptPot, HuntersPot, RefillPot, Biscuit, HPPot, Qss, Mercurial;
+
+        public static Item Youmus,
+            Botrk,
+            Bilgewater,
+            CorruptPot,
+            HuntersPot,
+            RefillPot,
+            Biscuit,
+            HPPot,
+            Qss,
+            Mercurial,
+            WardingTotem,
+            PinkVision,
+            GreaterVisionTotem,
+            GreaterStealthTotem,
+            FarsightAlteration;
         public static Spell.Active Heal, Barrier;
+        private const float WardRange = 1000f;
         public static void LoadSpells()
         {
             if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner1).Name.Contains("dot"))
@@ -29,9 +45,15 @@ namespace Boostana
             Mercurial = new Item((int)ItemId.Mercurial_Scimitar);
             HPPot = new Item(2003);
             Biscuit = new Item(2010);
-            CorruptPot = new Item(2033);
-            HuntersPot = new Item(2032);
             RefillPot = new Item(2031);
+            HuntersPot = new Item(2032);
+            CorruptPot = new Item(2033);
+            PinkVision = new Item(2043, WardRange);
+            GreaterStealthTotem = new Item(3361, WardRange);
+            GreaterVisionTotem = new Item(3362, WardRange);
+            FarsightAlteration = new Item(3363, WardRange);
+            WardingTotem = new Item(3340, WardRange);
+
         }
     }
 }
